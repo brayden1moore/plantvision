40dc14f8465380059a3741928f802942cb433601
Brayden Moore <98822684+brayden1moore@users.noreply.github.com>
2023-07-31 18:34:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def guess():
         return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
